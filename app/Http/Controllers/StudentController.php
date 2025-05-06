b8fdf0191eea9587307594431eec5650009fd0e6
SanSiro2603 <143419496+SanSiro2603@users.noreply.github.com>
2025-05-06 16:52:01 +0800

    part 3

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -22,17 +22,21 @@ class StudentController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('students.create');
+       
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request) : RedirectResponse
     {
-        //
+        $input = $request->all();
+        Student::create($input);
+        return redirect('students')->with('flash_message', 'Student Addedd!');
+        
     }
 
     /**
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -97,13 +97,14 @@
         <a href="#contact">Payment</a>
         </div>
     </div>
-    <div class="col-md 9">
-        <!-- Page content -->
-        <div class="content">
+    <div class="col-md-9">
+       
                      @yield('content')
         </div>
+
     </div>
-        </div>
+        
+</div>
 
 </body>
 </html>
