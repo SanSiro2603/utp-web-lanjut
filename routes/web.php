d6fd33e8441f46350ad66c4fabb6bc3f121a7988
SanSiro2603 <143419496+SanSiro2603@users.noreply.github.com>
2025-05-06 19:57:10 +0800

    part 5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,4 @@ Route::get('/', function () {
     return view('layout');
 });
 Route::resource('/students', StudentController::class);
+Route::resource('/teachers', TeacherController::class);
